433f986b7bdd93f110cb81e9f713e3fb277b29e6
MQG <mo1056610188@163.com>
2018-11-01 22:39:55 +0800

    logback.xml

--- src/main/java/com/momo/springbootlog/Application.java
+++ src/main/java/com/momo/springbootlog/Application.java
@@ -1,12 +1,16 @@
 package com.momo.springbootlog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		LOG.info("application run...");
 		SpringApplication.run(Application.class, args);
 	}
 }
